8b4de6283d1842635d6e00a81114682c776a7fe7
finn <finnickliu@tencent.com>
2020-12-07 22:29:45 +0800

    class 10 done

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { Table, Space } from 'antd';
+import { connect } from 'umi';
+import UserModal from './components/UserModal';
+
+const index = ({ users }) => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [record, setRecord] = useState(undefined);
+
+  const editHandler = record => {
+    setModalVisible(true);
+    setRecord(record);
+  };
+
+  const closeHandler = () => {
+    setModalVisible(false);
+  };
+
+  const columns = [
+    {
+      title: 'ID',
+      dataIndex: 'id',
+      key: 'id',
+    },
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      render: text => <a>{text}</a>,
+    },
+    {
+      title: 'Create Time',
+      dataIndex: 'create_time',
+      key: 'create_time',
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (text, record) => (
+        <Space size="middle">
+          <a
+            onClick={() => {editHandler(record)}}
+          >
+            Edit
+          </a>
+          <a>Delete</a>
+        </Space>
+      ),
+    },
+  ];
+
+  return (
+    <div className="list-table">
+      <Table columns={columns} dataSource={users.data} />
+      <UserModal
+        visible={modalVisible}
+        closeHandler={closeHandler}
+        record={record}
+      ></UserModal>
+    </div>
+  );
+};
+
+const mapStateToProps = ({ users }) => {
+  return {
+    users,
+  };
+};
+
+export default connect(mapStateToProps)(index);
---
+++ src/pages/users/model.ts
@@ -0,0 +1,83 @@
+import { Reducer, Effect, Subscription } from 'umi';
+import { getRemoteList } from './service';
+
+interface UserModelType {
+  namespace: 'users',
+  state: {},
+  reducers: {
+    getList: Reducer,
+  },
+  effects: {
+    getRemote: Effect,
+  },
+  subscriptions: {
+    setup: Subscription,
+  },
+}
+
+const UserModel: UserModelType = {
+  // 命名空间，这个model的唯一标识名
+  namespace: 'users',
+
+  // 仓库初始值
+  state: {},
+
+  // reducers 同步
+  reducers: {
+    getList(state, { payload }) {
+      // state 上一次的数据
+      //   const data = [
+      //     {
+      //       key: '1',
+      //       name: 'John Brown',
+      //       age: 32,
+      //       address: 'New York No. 1 Lake Park',
+      //       tags: ['nice', 'developer'],
+      //     },
+      //     {
+      //       key: '2',
+      //       name: 'Jim Green',
+      //       age: 42,
+      //       address: 'London No. 1 Lake Park',
+      //       tags: ['loser'],
+      //     },
+      //     {
+      //       key: '3',
+      //       name: 'Joe Black',
+      //       age: 32,
+      //       address: 'Sidney No. 1 Lake Park',
+      //       tags: ['cool', 'teacher'],
+      //     },
+      //   ];
+      console.log('reduces ->', payload);
+      return payload;
+    },
+  },
+  // effects 异步
+  effects: {
+    *getRemote(action, { put, call }) {
+      // yield put()
+      const data = yield call(getRemoteList);
+      console.log('data->', data);
+      yield put({
+        type: 'getList',
+        payload: data,
+      });
+    },
+  },
+
+  // 订阅
+  subscriptions: {
+    setup({ dispatch, history }) {
+      return history.listen(({ pathname }) => {
+        if (pathname === '/users') {
+          dispatch({
+            type: 'getRemote',
+          });
+        }
+      });
+    },
+  },
+};
+
+export default UserModel;
